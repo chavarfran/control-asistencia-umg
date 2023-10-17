9b0917eac9965df3af6418e850dbba7aaf8008f0
Francisco Chavarria <fchavarriar@outlook.com>
2023-10-17 00:53:17 -0600

    migraciones

--- app/Http/Livewire/Career/Table.php
+++ app/Http/Livewire/Career/Table.php
@@ -3,11 +3,21 @@
 namespace App\Http\Livewire\Career;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\DB;
 
 class Table extends Component
 {
+    public $career;  // Propiedad para almacenar los datos de la tabla faculty
+
+    public function mount()  // Método que se ejecuta cuando se crea el componente
+    {
+        $this->career = DB::table('tb_career')->get();  // Obtiene todos los registros de la tabla faculty
+    }
+
     public function render()
     {
-        return view('livewire.career.table');
+        return view('livewire.career.table', [
+            'careers' => $this->career  // Pasa los datos a la vista
+        ]);
     }
 }
