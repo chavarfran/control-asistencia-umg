cfbd2211c68aef7b2142b3617ebe8b97a891c020
Francisco Chavarria <fchavarriar@outlook.com>
2023-10-17 02:39:31 -0600

    create de career

--- app/Http/Livewire/Career/Table.php
+++ app/Http/Livewire/Career/Table.php
@@ -8,16 +8,39 @@ use Illuminate\Support\Facades\DB;
 class Table extends Component
 {
     public $career;  // Propiedad para almacenar los datos de la tabla faculty
+    public $faculties; 
+
+    public $nombre;
+    public $descripcion;
+    public $id_faculty = '';
 
     public function mount()  // Método que se ejecuta cuando se crea el componente
     {
         $this->career = DB::table('tb_career')->get();  // Obtiene todos los registros de la tabla faculty
+        $this->faculties = DB::table('tb_faculty')->get(); 
+    }
+
+    public function saveCareer()
+    {
+        DB::table('tb_career')->insert([
+            'nombre' => $this->nombre,
+            'descripcion' => $this->descripcion,
+            'id_faculty' => $this->id_faculty, // Si necesitas insertar el ID de la facultad o algo relacionado, ajusta según tu tabla y columnas.
+        ]);
+    
+        // Puedes emitir un evento o mostrar un mensaje de éxito aquí si lo deseas
+        $this->reset(['nombre', 'descripcion', 'id_faculty']); // Restablece las propiedades para limpiar el formulario
+    }
+
+    public function updateFacultyId($value)
+    {
+        $this->id_faculty = $value;
     }
 
     public function render()
     {
         return view('livewire.career.table', [
-            'careers' => $this->career  // Pasa los datos a la vista
+            'careers' => $this->career, 'faculties' => $this->faculties  // Pasa los datos a la vista
         ]);
     }
 }
--- resources/views/livewire/career/table.blade.php
+++ resources/views/livewire/career/table.blade.php
@@ -1,59 +1,31 @@
 <div class="col">
     <div class="row">
-        <div class="col-md-2">
-            <div class="card">
-                <div class="card-header mx-4 p-3 text-center">
-                    <div class="icon icon-shape icon-lg bg-gradient-info shadow text-center border-radius-lg">
-                        <i class="fas fa-landmark opacity-10"></i>
+        @foreach ($careers as $career)
+            <div class="col-md-2">
+                <div class="card">
+                    <div class="card-header mx-4 p-3 text-center">
+                        <div class="icon icon-shape icon-lg bg-gradient-info shadow text-center border-radius-lg">
+                            <i class="fas fa-landmark opacity-10"></i>
+                        </div>
                     </div>
-                </div>
-                <div class="card-body pt-0 p-3 text-center">
-                    <h6 class="text-center mb-0">Ingenieria en Sistemas de la Información y Ciencia</h6>
-                    <span class="text-xs">Facultad de Ingenieria</span>
-                    <hr class="horizontal dark my-3">
-                    <h5 class="mb-0">Descripción</h5>
-                    <span class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam maxime neque,
-                        voluptates soluta, odio quaerat nemo omnis delectus doloribus officia accusantium nisi dicta.
-                        Earum nobis laborum, sit corporis vero eum!</span>
-                </div>
-                <div class="d-flex justify-content-center align-items-center p-4">
-                    <div class="ms-auto">
-                        <a class="btn bg-gradient-danger px-3 mb-0"
-                            href="javascript:;"><i class="far fa-trash-alt me-2"></i>Eliminar</a>
-                        <a class="btn bg-gradient-dark px-3 mb-0" href="javascript:;"><i
-                                class="fas fa-pencil-alt me-2"
-                                aria-hidden="true"></i>Editar</a>
+                    <div class="card-body pt-0 p-3 text-center">
+                        <h6 class="text-center mb-0">{{ $career->nombre }}</h6>
+                        <span class="text-xs">Facultad de Ingenieria</span>
+                        <hr class="horizontal dark my-3">
+                        <h5 class="mb-0">Descripción</h5>
+                        <span class="text-xs">{{ $career->descripcion }}</span>
                     </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card">
-                <div class="card-header mx-4 p-3 text-center">
-                    <div class="icon icon-shape icon-lg bg-gradient-info shadow text-center border-radius-lg">
-                        <i class="fas fa-landmark opacity-10"></i>
-                    </div>
-                </div>
-                <div class="card-body pt-0 p-3 text-center">
-                    <h6 class="text-center mb-0">Licenciatura en Ciencias Jurídicas y Sociales</h6>
-                    <span class="text-xs">Ciencias Juridicas y Sociales</span>
-                    <hr class="horizontal dark my-3">
-                    <h5 class="mb-0">Descripción</h5>
-                    <span class="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, quia quasi
-                        molestiae quisquam vitae, placeat consequatur reprehenderit maiores quos eligendi adipisci iure
-                        sunt, repellendus consequuntur! Iure deserunt rerum tempora asperiores!</span>
-                </div>
-                <div class="d-flex justify-content-center align-items-center p-4">
-                    <div class="ms-auto">
-                        <a class="btn bg-gradient-danger px-3 mb-0"
-                            href="javascript:;"><i class="far fa-trash-alt me-2"></i>Eliminar</a>
-                        <a class="btn bg-gradient-dark px-3 mb-0" href="javascript:;"><i
-                                class="fas fa-pencil-alt me-2"
-                                aria-hidden="true"></i>Editar</a>
+                    <div class="d-flex justify-content-center align-items-center p-4">
+                        <div class="ms-auto">
+                            <a class="btn bg-gradient-danger px-3 mb-0" href="javascript:;"><i
+                                    class="far fa-trash-alt me-2"></i>Eliminar</a>
+                            <a class="btn bg-gradient-dark px-3 mb-0" href="javascript:;"><i
+                                    class="fas fa-pencil-alt me-2" aria-hidden="true"></i>Editar</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endforeach
         <div class="col-xl-2 col-md-4 mb-xl-0 mb-4">
             <div class="card h-100 card-plain border">
                 <div class="card-body d-flex flex-column justify-content-center text-center">
@@ -65,8 +37,8 @@
             </div>
         </div>
         <div class="col-md-4">
-            <div class="modal fade" id="modal-form" tabindex="-1" role="dialog"
-                aria-labelledby="modal-form" aria-hidden="true">
+            <div class="modal fade" id="modal-form" tabindex="-1" role="dialog" aria-labelledby="modal-form"
+                aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                     <div class="modal-content">
                         <div class="modal-body p-0">
@@ -77,22 +49,25 @@
                                     <p class="mb-0">Ingresar los campos requeridos</p>
                                 </div>
                                 <div class="card-body">
-                                    <form role="form text-left">
+                                    <form action="{{ route('career-store') }}" method="POST">
+                                        @csrf
                                         <label>Nombre</label>
                                         <div class="input-group mb-3">
-                                            <input type="text" class="form-control"  id="nombre"
+                                            <input type="text" class="form-control" name="nombre"
                                                 placeholder="nombre de la carrera">
                                         </div>
                                         <label>Descripción</label>
                                         <div class="form-group">
-                                            <textarea class="form-control" id="description" placeholder="Descripción de la carrera"></textarea>
+                                            <textarea class="form-control" name="descripcion" placeholder="Descripción de la carrera"></textarea>
                                         </div>
                                         <label>Facultad</label>
-                                        <select class="form-control">
-                                            <option>Seleccione una facultad</option>
-                                          </select>
+                                        <select class="form-control" name="id_faculty">
+                                            @foreach ($faculties as $facultie)
+                                                <option value="{{ $facultie->id }}">{{ $facultie->nombre }}</option>
+                                            @endforeach
+                                        </select>
                                         <div class="text-center">
-                                            <button type="button"
+                                            <button type="submit"
                                                 class="btn btn-round bg-gradient-info btn-lg w-100 mt-4 mb-0">Guardar</button>
                                         </div>
                                     </form>
