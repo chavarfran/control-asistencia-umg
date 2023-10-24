e82b24278174c5f802e54dd5c9b0748a1079c8aa
Francisco Chavarria <fchavarriar@outlook.com>
2023-10-24 00:21:54 -0600

    seleccionable de edit

--- app/Http/Livewire/Pensum/Edit.php
+++ app/Http/Livewire/Pensum/Edit.php
@@ -3,15 +3,22 @@
 namespace App\Http\Livewire\Pensum;
 
 use Livewire\Component;
+use App\Models\Career;
+use App\Models\Faculty;
 use Illuminate\Support\Facades\DB;
 
 class Edit extends Component
 {
-    protected $pensum;
-    
+    protected $pensum;  // Almacena el pensum actual para edición
+    public $career=[];      // Propiedad para almacenar los datos de las carreras
+    public $faculties=[];   // Propiedad para almacenar los datos de las facultades
+    public $id_faculty;     // Propiedad para almacenar el id de la facultad seleccionada
+
     public function mount()  // Aceptamos el ID del pensum como parámetro
     {
         $id = request()->query('id');
+
+        $this->faculties = Faculty::all();
 
         $this->pensum = DB::table('tb_pensum')
             ->where('tb_pensum.id', '=', $id)
@@ -19,13 +26,23 @@ class Edit extends Component
             ->join('tb_faculty', 'tb_career.id_faculty', '=', 'tb_faculty.id')
             ->select('tb_pensum.*', 'tb_career.id as id_carrera', 'tb_career.nombre_carrera', 'tb_career.id as id_facultad', 'tb_faculty.nombre_facultad')
             ->first();
+
+        // Establecemos las carreras basadas en la facultad inicial del pensum actual
+        $this->updatedIdFaculty($this->pensum->id_facultad);
+    }
+
+    public function updatedIdFaculty($newValue)
+    {
+        // Actualiza la lista de carreras basada en la facultad seleccionada
+        $this->career = Career::where('id_faculty', $newValue)->get();
     }
 
     public function render()
     {
-        //dd($this->pensum[0]);
         return view('livewire.pensum.edit', [
-            'pensums' => $this->pensum,
+            'pensums' => $this->pensum,  // Pasamos el pensum actual a la vista
+            'careers' => $this->career,         // Pasamos las carreras a la vista
+            'faculties' => $this->faculties,    // Pasamos las facultades a la vista
         ]);
     }
 }
--- resources/views/livewire/pensum/edit.blade.php
+++ resources/views/livewire/pensum/edit.blade.php
@@ -1,5 +1,5 @@
 <div class="container py-4">
-    <div class="row justify-content-center"> <!-- Centralizar el contenido en la fila -->
+    <div class="row justify-content-center">
         <div class="col-lg-6 col-md-8 col-sm-12">
             <div class="card">
                 <div class="card-header pb-0 px-3">
@@ -12,39 +12,56 @@
                             <div class="form-group">
                                 <label for="pensums-nombre_pensum"
                                     class="form-control-label">{{ __('Nombre de pensum') }}</label>
-                                <div class="@error('pensu.nombre_pensum')border border-danger rounded-3 @enderror">
+                                <div class="@error('nombre_pensum')border border-danger rounded-3 @enderror">
                                     <input class="form-control" type="text" placeholder="Nombre de pensum"
-                                        name="nombre_pensum"
-                                        value="{{ $pensums->nombre_pensum }}">
+                                        name="nombre_pensum" value="{{ $pensums->nombre_pensum ?? '' }}">
                                 </div>
                                 @error('nombre_pensum')
                                     <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
+
                             <div class="form-group">
                                 <label for="pensums.id_faculty" class="form-control-label">{{ __('Facultad') }}</label>
-                                <div class="@error('pensums.id_faculty')border border-danger rounded-3 @enderror">
-                                    <select class="form-control" name="id_facultad">
-                                        <option value="{{ $pensums->id_facultad }}">
-                                            {{ $pensums->nombre_facultad }}</option>
+                                <div class="@error('id_facultad')border border-danger rounded-3 @enderror">
+                                    <select wire:model="id_faculty" class="form-control" name="id_facultad">
+                                        <option value="{{ $pensums->id_facultad ?? '' }}">
+                                            {{ $pensums ? 'ORIGINAL - ' . $pensums->nombre_facultad : '' }}
+                                        </option>
+                                        @foreach ($faculties as $faculty)
+                                            <!-- Evita mostrar la opción duplicada para la facultad original -->
+                                            @if ($pensums && $faculty->id !== $pensums->id_facultad)
+                                                <option value="{{ $faculty->id }}">
+                                                    {{ $faculty->nombre_facultad }}
+                                                </option>
+                                            @endif
+                                        @endforeach
                                     </select>
                                 </div>
-                                @error('id_faculty')
+                                @error('id_facultad')
                                     <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
+
                             <div class="form-group">
                                 <label for="pensums.id_career" class="form-control-label">{{ __('Carrera') }}</label>
-                                <div class="@error('pensum.id_career') border border-danger rounded-3 @enderror">
+                                <div class="@error('id_carrera') border border-danger rounded-3 @enderror">
                                     <select class="form-control" name="id_carrera">
-                                        <option value="{{ $pensums->id_carrera }}">
-                                            {{ $pensums->nombre_carrera }}</option>
+                                        <option value="{{ $pensums->id_carrera ?? '' }}">
+                                            {{ $pensums ? 'ORIGINAL - ' . $pensums->nombre_carrera : '' }}
+                                        </option>
+                                        @foreach ($careers as $career)
+                                            <option value="{{ $career->id }}">
+                                                {{ $career->nombre_carrera }}
+                                            </option>
+                                        @endforeach
                                     </select>
                                 </div>
-                                @error('id_career')
+                                @error('id_carrera')
                                     <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
+
                         </div>
                         <div class="d-flex justify-content-end">
                             <button type="submit"
