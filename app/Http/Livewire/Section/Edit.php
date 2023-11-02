ffa2cfe506c882dfa97dee63d8be62e6147c1b98
Francisco Chavarria <fchavarriar@outlook.com>
2023-11-01 23:30:26 -0600

    creando componentes

---
+++ app/Http/Livewire/Section/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Section;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.section.edit');
+    }
+}
