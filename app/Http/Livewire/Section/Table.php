af5fdbaa11d8e76c329fc985ecf4530e06c8bd55
Francisco Chavarria <fchavarriar@outlook.com>
2023-11-01 23:04:03 -0600

    componenete livewire section

--- app/Http/Livewire/Section/Table.php
+++ app/Http/Livewire/Section/Table.php
@@ -6,8 +6,25 @@ use Livewire\Component;
 
 class Table extends Component
 {
+    use WithPagination; 
+
+    protected $section;
+
+    public function mount()  // Método que se ejecuta cuando se crea el componente
+    {
+        $this->section = DB::table('tb_section')
+            ->join('tb_semester', 'tb_section.id_semestre', '=', 'tb_semester.id')
+            ->join('tb_pensum', 'tb_semester.id_pensum', '=', 'tb_pensum.id')
+            ->join('tb_career', 'tb_pensum.id_carrera', '=', 'tb_career.id')
+            ->join('tb_faculty', 'tb_career.id_facultad', '=', 'tb_faculty.id')
+            ->select('tb_section.*', 'tb_semester.nombre_semestre', 'tb_pensum.nombre_pensum', 'tb_career.nombre_carrera', 'tb_faculty.nombre_facultad')
+            ->paginate(10);
+    }
+
     public function render()
     {
-        return view('livewire.section.table');
+        return view('livewire.section.table', [
+            'sections' => $this->section  // Pasa los datos a la vista
+        ]);
     }
 }
