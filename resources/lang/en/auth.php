5c2831248cf8097d181716dae6217954af67dc51
unknown <mlopezb11@miumg.edu.gt>
2023-11-03 04:58:48 -0600

    traduccion de validacion de campos

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Usuario o contraseña proporcionada es incorrecta.',
+    'password' => 'Usuario o contraseña proporcionada es incorrecta.',
+    'throttle' => 'Demasiados intentos de inicio de sesión. Inténtelo de nuevo en:seconds segundos.',
 
 ];
