1d1c10e9f0ebce4c29bd556665ef6d3b015e593d
Francisco Chavarria <fchavarriar@outlook.com>
2023-11-06 23:50:22 -0600

    menu de asistencia

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -5,7 +5,7 @@
             <img src="../assets/img/logo_white.png" class="full-width-height" alt="...">
         </a>
     </div>
-    <hr class="horizontal dark mt-0">   
+    <hr class="horizontal dark mt-0">
     <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item pb-2">
@@ -97,6 +97,17 @@
                         </svg>
                     </div>
                     <span class="nav-link-text ms-1">Perfil Usuario</span>
+                </a>
+            </li>
+            <li class="nav-item pb-2">
+                <a class="nav-link {{ Route::currentRouteName() == 'tabla-facultad' ? 'active' : '' }}"
+                    href="{{ route('tabla-facultad') }}">
+                    <div
+                        class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i style="font-size: 1rem;" class="fas fa-lg fa-chalkboard-teacher ps-2 pe-2 text-center
+                        {{ in_array(request()->route()->getName(),['tabla-facultad']) ? 'text-white' : 'text-dark' }}"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Asistencia</span>
                 </a>
             </li>
             <li class="nav-item pb-2">
