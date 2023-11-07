85247a8f2546b7adfb78d46adf2f1fb4c28127fb
Francisco Chavarria <fchavarriar@outlook.com>
2023-11-06 23:19:52 -0600

    crear componentes de assistence

---
+++ app/Http/Livewire/Assistance/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Assistance;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.assistance.create');
+    }
+}
---
+++ app/Http/Livewire/Assistance/Table.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Assistance;
+
+use Livewire\Component;
+
+class Table extends Component
+{
+    public function render()
+    {
+        return view('livewire.assistance.table');
+    }
+}
