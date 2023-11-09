ae77321fb001d2a055e2b592abafacd4f0740eba
Francisco Chavarria <fchavarriar@outlook.com>
2023-11-09 03:45:51 -0600

    estructura de topics

---
+++ app/Http/Livewire/Topics/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Topics;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.topics.edit');
+    }
+}
