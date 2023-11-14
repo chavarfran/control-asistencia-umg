0e653efa35129efa82ee5a125daa41147abe1add
Francisco Chavarria <fchavarriar@outlook.com>
2023-11-14 12:14:42 -0600

    estilos y diseño

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,1 +1,300 @@
-<div>hola umg</div>
+<main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+    <div class="container py-4">
+        <div class="row">
+            <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="numbers">
+                                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Today's Money</p>
+                                    <h5 class="font-weight-bolder mb-0">
+                                        $53,000
+                                        <span class="text-success text-sm font-weight-bolder">+55%</span>
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="col-4 text-end">
+                                <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                    <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="numbers">
+                                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Today's Users</p>
+                                    <h5 class="font-weight-bolder mb-0">
+                                        2,300
+                                        <span class="text-success text-sm font-weight-bolder">+3%</span>
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="col-4 text-end">
+                                <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                    <i class="ni ni-world text-lg opacity-10" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="numbers">
+                                    <p class="text-sm mb-0 text-capitalize font-weight-bold">New Clients</p>
+                                    <h5 class="font-weight-bolder mb-0">
+                                        +3,462
+                                        <span class="text-danger text-sm font-weight-bolder">-2%</span>
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="col-4 text-end">
+                                <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                    <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="numbers">
+                                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Sales</p>
+                                    <h5 class="font-weight-bolder mb-0">
+                                        $103,430
+                                        <span class="text-success text-sm font-weight-bolder">+5%</span>
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="col-4 text-end">
+                                <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                    <i class="ni ni-cart text-lg opacity-10" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-lg-7 mb-lg-0 mb-4">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="d-flex flex-column h-50">
+                                    <p class="mb-1 pt-2 text-bold">Sistema de control de asistencia
+                                    </p>
+                                    <h5 class="font-weight-bolder">Universidad mariano galvez</h5>
+                                    <p class="mb-5">¡Presentamos nuestro innovador Sistema de Asistencia Inteligente
+                                        para Universidad Mariano Galvez! Diseñado para revolucionar la manera en que se
+                                        gestiona la asistencia de catedráticos, nuestro sistema ofrece una solución
+                                        integral que combina eficiencia, precisión y facilidad de uso.</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-5 ms-auto text-center mt-5 mt-lg-0">
+                                <div class="bg-gradient-warning border-radius-lg h-100">
+                                    <img src="../assets/img/dashboard mision.jpg"
+                                        class="position-absolute h-100 w-50 top-0 d-lg-block d-none" alt="waves">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <div class="card h-100 p-3">
+                    <div class="overflow-hidden position-relative border-radius-lg bg-cover h-100"
+                        style="background-image: url('../assets/img/ivancik.jpg');">
+                        <span class="mask bg-gradient-dark"></span>
+                        <div class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3">
+                            <h5 class="text-white font-weight-bolder mb-4 pt-2">Transforma la Gestión de Asistencia en
+                                tu Universidad</h5>
+                            <p class="text-white">Con nuestro Sistema de Asistencia Inteligente, da un paso hacia el
+                                futuro de la educación. Mejora la eficiencia, la precisión y la comunicación en tu
+                                institución. ¡Únete a la revolución de la gestión académica inteligente hoy mismo!</p>
+                            <a class="text-white text-sm font-weight-bold mb-0 icon-move-right mt-auto"
+                                href="javascript:;">
+                                Leer más
+                                <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<!--   Core JS Files   -->
+<script src="/assets/js/plugins/chartjs.min.js"></script>
+<script src="/assets/js/plugins/Chart.extension.js"></script>
+<script>
+    var ctx = document.getElementById("chart-bars").getContext("2d");
+
+    new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            datasets: [{
+                label: "Sales",
+                tension: 0.4,
+                borderWidth: 0,
+                pointRadius: 0,
+                backgroundColor: "#fff",
+                data: [450, 200, 100, 220, 500, 100, 400, 230, 500],
+                maxBarThickness: 6
+            }, ],
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+                display: false,
+            },
+            tooltips: {
+                enabled: true,
+                mode: "index",
+                intersect: false,
+            },
+            scales: {
+                yAxes: [{
+                    gridLines: {
+                        display: false,
+                    },
+                    ticks: {
+                        suggestedMin: 0,
+                        suggestedMax: 500,
+                        beginAtZero: true,
+                        padding: 0,
+                        fontSize: 14,
+                        lineHeight: 3,
+                        fontColor: "#fff",
+                        fontStyle: 'normal',
+                        fontFamily: "Open Sans",
+                    },
+                }, ],
+                xAxes: [{
+                    gridLines: {
+                        display: false,
+                    },
+                    ticks: {
+                        display: false,
+                        padding: 20,
+                    },
+                }, ],
+            },
+        },
+    });
+
+    var ctx2 = document.getElementById("chart-line").getContext("2d");
+
+    var gradientStroke1 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+    gradientStroke1.addColorStop(1, 'rgba(253,235,173,0.4)');
+    gradientStroke1.addColorStop(0.2, 'rgba(245,57,57,0.0)');
+    gradientStroke1.addColorStop(0, 'rgba(255,214,61,0)'); //purple colors
+
+    var gradientStroke2 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+    gradientStroke2.addColorStop(1, 'rgba(20,23,39,0.4)');
+    gradientStroke2.addColorStop(0.2, 'rgba(245,57,57,0.0)');
+    gradientStroke2.addColorStop(0, 'rgba(255,214,61,0)'); //purple colors
+
+
+    new Chart(ctx2, {
+        type: "line",
+        data: {
+            labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            datasets: [{
+                    label: "Mobile apps",
+                    tension: 0.4,
+                    borderWidth: 0,
+                    pointRadius: 0,
+                    borderColor: "#fbcf33",
+                    borderWidth: 3,
+                    backgroundColor: gradientStroke1,
+                    data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+                    maxBarThickness: 6
+
+                },
+                {
+                    label: "Websites",
+                    tension: 0.4,
+                    borderWidth: 0,
+                    pointRadius: 0,
+                    borderColor: "#f53939",
+                    borderWidth: 3,
+                    backgroundColor: gradientStroke2,
+                    data: [30, 90, 40, 140, 290, 290, 340, 230, 400],
+                    maxBarThickness: 6
+
+                },
+            ],
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+                display: false,
+            },
+            tooltips: {
+                enabled: true,
+                mode: "index",
+                intersect: false,
+            },
+            scales: {
+                yAxes: [{
+                    gridLines: {
+                        borderDash: [2],
+                        borderDashOffset: [2],
+                        color: '#dee2e6',
+                        zeroLineColor: '#dee2e6',
+                        zeroLineWidth: 1,
+                        zeroLineBorderDash: [2],
+                        drawBorder: false,
+                    },
+                    ticks: {
+                        suggestedMin: 0,
+                        suggestedMax: 500,
+                        beginAtZero: true,
+                        padding: 10,
+                        fontSize: 11,
+                        fontColor: '#adb5bd',
+                        lineHeight: 3,
+                        fontStyle: 'normal',
+                        fontFamily: "Open Sans",
+                    },
+                }, ],
+                xAxes: [{
+                    gridLines: {
+                        zeroLineColor: 'rgba(0,0,0,0)',
+                        display: false,
+                    },
+                    ticks: {
+                        padding: 10,
+                        fontSize: 11,
+                        fontColor: '#adb5bd',
+                        lineHeight: 3,
+                        fontStyle: 'normal',
+                        fontFamily: "Open Sans",
+                    },
+                }, ],
+            },
+        },
+    });
+</script>
