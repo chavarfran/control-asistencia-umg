4de11274c01569820968b21feb6500c1c49ed65c
Francisco Chavarria <fchavarriar@outlook.com>
2023-11-14 10:48:50 -0600

    encabeza

--- resources/views/livewire/reports/asistencia.blade.php
+++ resources/views/livewire/reports/asistencia.blade.php
@@ -8,118 +8,67 @@
     <title>Reporte</title>
     <style>
         @page {
-            /*margin-top: 2.5cm;*/
-            margin-left: 1.0cm;
-            margin-bottom: 1.0cm;
-            margin-right: 1.0cm;
-            line-height: 1.5;
+            margin-top: 1.5cm;
+            margin-left: 2.0cm;
+            margin-bottom: 1.5cm;
+            margin-right: 2.0cm;
+            line-height: 1.0;
             font-size: 12;
             text-align: justify;
-            font-family: Arial, Helvetica, sans-serif;
+            font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif ;
         }
 
-        th,td {
-            border-style: hidden;
+        img {
+            max-width: 100px;
+            height: auto;
         }
 
-        table {
-            table-layout: fixed;
-            width: 100%;
-            border-collapse: collapse;
-
+        h5 {
+            font-weight: normal;
+            line-height: 0;
         }
 
-        tr:not(:last-child) {
-            display: none;
+        h4 {
+            line-height: 0;
+        }
+
+        h3 {
+            line-height: 0;
         }
 
         .header {
-            text-align: center;
-            margin-bottom: 5px;
-            font-size: 25px;
-          
-        }
-
-        .header_2 {
-            text-align: left;
-            margin-bottom: 5px;
-            font-size: 15px;
-            text-align: center;
-        }
-
-        .form-label {
-            font-style: italic;
-            padding: 4px;
-            caption-side: bottom;
-            text-align: left;
-            letter-spacing: 1px;
-            background-color: #E2474B;
-            color: white;
-        }
-        .header-container {
-            display: block;
+            display: flex;
             align-items: center;
-            justify-content: flex-end;
-        }
-        .logo {
-            margin-right: 10px;
-        }
-        img {
-            width: 80px;
-            height: 80px;
+            justify-content: center;
+            text-align: center;
         }
 
+        .logo {
+            position: absolute;
+            max-width: 120px;
+            height: auto;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+        }
+
+        .title-head {
+            margin-left: 10px;
+        }
     </style>
 </head>
 
 <body>
-    <div class="header-container">
-        <h1 class="logo">
-            <img src="{{ public_path('/assets/img/logoUMG.png') }}">
-        </h1>
-        <h1 class="header">Universidad Mariano Galvez de Guatemala</h1>
+    <div class="header">
+        <img src="{{ public_path('/assets/img/logoUMG.png') }}" class="logo">
+        <div class="title-head">
+            <h3>Universidad Mariano Gálvez de Guatemala</h3>
+            <h5>ASISTENCIA CATEDRÁTICOS</h5>
+            <h5>Centro universitario de:</h5>
+            <h5><strong><u>COBÁN, ALTA VERAPAZ</u></strong></h5>
+            <h5>INGENIERIA EN SISTEMAS PLAN FIN DE SEMANA</h5>
+        </div>
     </div>
-
-    <h3 class="header_2">Coban, Alta Verapaz</h3>
-    <h3 class="header_2">Ingenieria en Sistemas y Ciencias de la Computación</h3>
-    <table border="1">
-        <tr>
-            <th class="form-label">Catedrático</th>
-            <th class="form-label">Curso</th>
-            <th class="form-label">Fecha/Horario</th>
-            <th class="form-label">Tema a trabajar</th>
-            <th class="form-label">Código</th>
-            <th class="form-label">Observaciones</th>
-        </tr>
-        <tr>
-            <td>Anthony Alexander Morales
-                <div>
-
-                </div>
-            </td>
-            <td>Curso A</td>
-            <td>11/10/2023 10:00AM-12:00PM</td>
-            <td>Tema 1</td>
-            <td>001</td>
-            <td>Observación 1</td>
-        </tr>
-        <tr>
-            <td>Profesor B</td>
-            <td>Curso B</td>
-            <td>Miércoles 2:00 PM - 4:00 PM</td>
-            <td>Tema 2</td>
-            <td>002</td>
-            <td>Observación 2</td>
-        </tr>
-        <tr>
-            <td>Profesor C</td>
-            <td>Curso C</td>
-            <td>Viernes 8:00 AM - 10:00 AM</td>
-            <td>Tema 3</td>
-            <td>003</td>
-            <td>Observación 3</td>
-        </tr>
-    </table>
 </body>
 
 </html>
